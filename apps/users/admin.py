19a0a138160ce6e13aa157bbc83b00d7a965443b
jokerLiz <59729729+jokerLiz@users.noreply.github.com>
2020-04-29 16:32:01 +0800

    xadmin的配置

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 # Register your models here.
 
 from apps.users.models import UserProfile
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-admin.site.register(UserProfile,UserProfileAdmin)
+
+#自定义UserProfileAdmin注册
+# class UserProfileAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(UserProfile,UserProfileAdmin)
+
+#利用UserAdmin优化
+#导入UserAdmin
+from django.contrib.auth.admin import UserAdmin
+admin.site.register(UserProfile,UserAdmin)
