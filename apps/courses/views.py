bf5465fcc00b0c093021ca9c763161a2eadf6219
jokerLiz <59729729+jokerLiz@users.noreply.github.com>
2020-05-12 13:39:26 +0800

    courselist页面内容的分页显示
    1.显示课程的数量
    2.分页显示课程的内容。

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.views.generic import View   #视图类
 from apps.courses.models import *     #模型类
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger     #分页
+
 class CourseView(View):
     def get(self, request, *args, **kwargs):
         '''
@@ -13,10 +14,24 @@ class CourseView(View):
         :param kwargs:
         :return:
         '''
-        all_courses = Course.objects.all()
+        #查询数据库中的所有信息，按照时间从大到小排序
+        all_courses = Course.objects.all().order_by('-add_time')
+        #查询信息的个数
         course_nums = all_courses.count()
 
+        # 分页部分
+        # 获取page，如果没找到或者出错都置page为1
+        try:
+            pindex = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            pindex = 1
+
+        # 参数1：作用对象，参数2：单页显示数量，参数3：request
+        p = Paginator(all_courses, per_page=6, request=request)
+
+        courses = p.page(pindex)  # 获取pindex页的信息
+
         return render(request,'courselist.html',{
-            'all_courses':all_courses,
+            'all_courses':courses,
             'course_nums':course_nums
         })
