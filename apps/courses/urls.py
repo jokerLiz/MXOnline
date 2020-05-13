f53e4bf757cfbab34c4867cdb0b1c0d00ae2ffe4
jokerLiz <59729729+jokerLiz@users.noreply.github.com>
2020-05-13 14:53:59 +0800

    courselist页面筛选选项和排序的联动
    1.筛选选项使用课程类别进行筛选，循环所有课程的课程类别(去重)，循环显示到前端页面，把接口值设为循环项传给后端，后端根据值进行过滤。
    2.排序每一项设置两个接口--ct和sort，ct值就是用于联动的从后端传过来的筛选选项的值，sort分别为hot和students，传到后端进行处理排序，并把sort值在传回来用于高亮的显示。

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -3,5 +3,5 @@ from apps.courses.views import CourseView
 
 urlpatterns = [
     #公开课列表展示
-    url(r'^list/',CourseView.as_view(),name = 'list')
+    url(r'^list/$',CourseView.as_view(),name = 'list')
     ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -16,7 +16,30 @@ class CourseView(View):
         '''
         #查询数据库中的所有信息，按照时间从大到小排序
         all_courses = Course.objects.all().order_by('-add_time')
-        #查询信息的个数
+
+        #取出筛选选项,根据课程标签(category)进行筛选
+        cate_list = []
+        for course in all_courses:
+            if course.category not in cate_list:
+                cate_list.append(course.category)
+
+        #根据筛选选项筛选课程数据
+        cate_name = request.GET.get('ct','')   # 获取前台接口数据
+        if cate_name:
+            # 过滤类别为用户选择的数据
+            all_courses = all_courses.filter(category=cate_name)
+
+
+        #排序
+        sort = request.GET.get('sort','')
+        if sort == 'hot':
+            # 根据学生进行排序，-号代表倒序,一定要拿变量接着。
+            all_courses = all_courses.order_by('-click_nums')
+        elif sort == 'students':
+            # 根据课程数进行排序
+            all_courses = all_courses.order_by('-students')
+
+        #查询课程数据的个数
         course_nums = all_courses.count()
 
         # 分页部分
@@ -32,6 +55,9 @@ class CourseView(View):
         courses = p.page(pindex)  # 获取pindex页的信息
 
         return render(request,'courselist.html',{
-            'all_courses':courses,
-            'course_nums':course_nums,
+            'all_courses':courses,             #课程信息
+            'course_nums':course_nums,         #课程数量
+            'cate_list':cate_list,             #筛选选项列表
+            'cate_name':cate_name,             #点击的选项名，用于前端的高亮和联动
+            'sort':sort,                       #点击的排序规则，用于前端的高亮变换
         })
